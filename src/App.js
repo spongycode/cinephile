437e0ef7570ffe3c0b1596c00890d883b8290c3a
hkbettiah@gmail.com <hkbettiah@gmail.com>
2023-01-17 00:53:38 +0530

    fetch from api

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Box } from "@mui/material";
 
 import { Navbar, VideoDetail, SearchFeed, Feed, ChannelDetail } from './components'
+
 const App = () => (
     <BrowserRouter>
         <Box sx={{ backgroundColor: '#000' }}>
